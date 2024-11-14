2db8e09eec03ed7ce4a759aab2a42a321f7cdba4
unknown <Emmzyemms>
2024-11-14 17:41:14 +0100

    adding js file

---
+++ index.js
@@ -0,0 +1,9 @@
+O
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+})
+
